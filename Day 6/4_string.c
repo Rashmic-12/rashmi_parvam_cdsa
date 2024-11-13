09c80c74c6a338d08defb4c2eb28986c125eb7a3
Rashmic-12 <rashmicgowda12@gmail.com>
2024-11-13 11:02:45 +0530

    Delete Day 6 directory

--- Day 6/4_string.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    char a[10];
-    printf("enter the name:");
-    scanf("%s",a);
-    printf(" the name is :  %s",a);
-
-    return 0;
-}
--- Day 6/5_string2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    char a[20];
-    printf("enter the name:");
-         fgets(a,sizeof(a),stdin);
-      printf(" the name is :  %s",a);
-
-    return 0;
-}
